e16fcae5cfff363b0d3665ce75b65c8e673e416a
glbarreto <55640976+glbarreto@users.noreply.github.com>
2023-06-19 00:09:04 -0300

    Update gerador_entrada.py

--- gerador_entrada.py
+++ gerador_entrada.py
@@ -10,11 +10,31 @@ def generate_test_cases(num_cases, max_sequence_length, max_pedra_length):
     for _ in range(num_cases):
         sequence_length = random.randint(1, max_sequence_length)
         pedra_length = random.randint(sequence_length, max_pedra_length)
+
+        # Gera a sequência de habilidades
         sequence = generate_random_string(sequence_length)
+
+        # Gera a pedra
         pedra = generate_random_string(pedra_length)
-        start_index = random.randint(0, pedra_length - sequence_length)
-        end_index = start_index + sequence_length
-        pedra = pedra[:start_index] + sequence + pedra[end_index:]
+
+        # Insere a sequência de habilidades em algumas pedras
+        if random.random() < 0.6:  # 60% de chance de inserção da sequência
+            start_index = random.randint(0, pedra_length - sequence_length)
+            end_index = start_index + sequence_length
+            pedra = pedra[:start_index] + sequence + pedra[end_index:]
+
+        # Inverte a sequência de habilidades em algumas pedras
+        if random.random() < 0.3:  # 30% de chance de inversão da sequência
+            sequence = sequence[::-1]  # Inverte a sequência
+            start_index = random.randint(0, pedra_length - sequence_length)
+            end_index = start_index + sequence_length
+            pedra = pedra[:start_index] + sequence + pedra[end_index:]
+
+        # Concatena as strings da pedra e coloca a sequência no fim da string
+        if random.random() < 0.1:  # 10% de chance de concatenação invertida
+            pedra = pedra[::-1]  # Inverte a pedra
+            pedra += sequence
+
         test_cases.append((sequence, pedra))
     return test_cases
 
@@ -25,13 +45,13 @@ def write_test_cases_to_file(test_cases, filename):
             file.write(f"{sequence} {pedra}\n")
 
 # Configurações para gerar entradas aleatórias
-num_cases = 10
 max_sequence_length = 100
 max_pedra_length = 10000
 num_files = 10
 
 # Gera e salva os casos de teste em arquivos separados
 for i in range(num_files):
+    num_cases = 100
     test_cases = generate_test_cases(num_cases, max_sequence_length, max_pedra_length)
     filename = f"input_{i + 1}.txt"
     write_test_cases_to_file(test_cases, filename)
